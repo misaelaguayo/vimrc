877d77173d4d61c81bffaadc48c1cda81ba8439d
Misael Aguayo <aguayo.misael@gmail.com>
2023-03-24 06:04:51 -0500

    Update config.nix

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,1 +1,17 @@
-{ allowUnfree = true; }
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+        alacritty
+        docker
+        git
+        neovim
+        sudo
+        tmux
+        zsh
+      ];
+      pathsToLink = [ "/share" "/bin" "/Applications" ];
+    };
+  };
+}

244c6149477182e7958a32af970b3681b3ffe5f7
Misael Aguayo <aguayo.misael@gmail.com>
2023-10-02 17:05:12 -0500

    Format and add surround

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,11 +24,11 @@ local function map(mode, combo, action, opts)
     vim.api.nvim_set_keymap(mode, combo, action, options)
 end
 
-vim.keymap.set('n', '<CR>', ':noh<CR><CR>', {noremap = true})
-vim.keymap.set('n', '<Leader>b', ':Buffers<CR>', {noremap = true})
-vim.keymap.set('n', '<C-f>', ':Files<CR>', {noremap = true})
-vim.keymap.set('n', '<Leader>f', ':Rg<CR>', {noremap = true})
-vim.keymap.set('n', '<Leader>w', ':set wrap!<CR>', {noremap = true})
+vim.keymap.set('n', '<CR>', ':noh<CR><CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>b', ':Buffers<CR>', { noremap = true })
+vim.keymap.set('n', '<C-f>', ':Files<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>f', ':Rg<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>w', ':set wrap!<CR>', { noremap = true })
 
 -- disable netrw at the very
 vim.g.loaded_netrw = 1
@@ -39,14 +39,14 @@ vim.opt.termguicolors = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -59,12 +59,12 @@ require("lazy").setup({
     "mfussenegger/nvim-dap",
     "folke/which-key.nvim",
     "folke/trouble.nvim",
-    {"folke/neodev.nvim", opts={} },
+    { "folke/neodev.nvim",        opts = {} },
     "github/copilot.vim",
-    {"ellisonleao/gruvbox.nvim", priority = 1000},
+    { "ellisonleao/gruvbox.nvim", priority = 1000 },
     "ms-jpq/coq_nvim",
     "ms-jpq/coq.artifacts",
-    {"junegunn/fzf", run = "./install --all" },
+    { "junegunn/fzf", run = "./install --all" },
     "junegunn/fzf.vim",
     "tpope/vim-fugitive",
     "lewis6991/gitsigns.nvim",
@@ -77,6 +77,14 @@ require("lazy").setup({
     "hrsh7th/nvim-cmp",
     "quangnguyen30192/cmp-nvim-ultisnips",
     "numToStr/Comment.nvim",
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({})
+        end,
+    },
     build = ":MasonUpdate" -- :MasonUpdate updates registry contents
 })
 
@@ -93,15 +101,15 @@ cmp.setup({
         documentation = cmp.config.window.bordered()
     },
     mapping = cmp.mapping.preset.insert({
-      ['<C-p>'] = cmp.mapping.select_prev_item(),
-      ['<C-n>'] = cmp.mapping.select_next_item(),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'ultisnips' },
+        { name = 'nvim_lsp' },
+        { name = 'ultisnips' },
     }, {})
 })
 
@@ -117,7 +125,7 @@ require("mason-lspconfig").setup_handlers {
     -- The first entry (without a key) will be the default handler
     -- and will be called for each installed server that doesn't have
     -- a dedicated handler.
-    function (server_name) -- default handler (optional)
+    function(server_name)  -- default handler (optional)
         require("lspconfig")[server_name].setup {
             capabilities = capabilities,
         }
@@ -144,37 +152,38 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 -- gitsigns keybindings
 vim.keymap.set('n', '<space>gj', ':Gitsigns next_hunk<CR>')
+vim.keymap.set('n', '<space>gp', ':Gitsigns preview_hunk<CR>')
 vim.keymap.set('n', '<space>gk', ':Gitsigns prev_hunk<CR>')
 vim.keymap.set('n', '<space>gr', ':Gitsigns reset_hunk<CR>')
 vim.keymap.set('n', '<space>gR', ':Gitsigns reset_buffer<CR>')
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
 })
 
-vim.cmd[[colorscheme gruvbox]]
+vim.cmd [[colorscheme gruvbox]]

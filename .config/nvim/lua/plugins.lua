f216379c7e98096d5a4cb78738ce010f79dd2aa0
Misael Aguayo <aguayo.misael@gmail.com>
2025-09-24 17:46:20 -0500

    First configuration.nix

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,8 @@
 return {
+    {"3rd/image.nvim", build = false, config = function() require("image").setup({
+	backend = "kitty",
+	processor = "magick_cli",
+    }) end},
     "github/copilot.vim",
     {
         "folke/noice.nvim",
